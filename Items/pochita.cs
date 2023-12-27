17dfb840c6d479ced92c0bb78ee790e669e4c8e8
ducktah <154904989+ducktah@users.noreply.github.com>
2023-12-28 00:03:51 +0800

    Update pochita.cs
    標示註解

--- Items/pochita.cs
+++ Items/pochita.cs
@@ -7,7 +7,7 @@ using trmod.Projectils;
 
 namespace trmod.Items
 {
-	public class pochita : ModItem //skip is a class of trmod.Items same as ModItem from trmodloarder
+	public class pochita : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.trmod.hjson file.
 
@@ -15,21 +15,21 @@ namespace trmod.Items
 		{
 			Item.useStyle = 5;   //使用方式
 			Item.useAnimation = 24;
-			Item.useTime = 13;
-			Item.shootSpeed = 40f;
+			Item.useTime = 13;	//攻擊間隔
+			Item.shootSpeed = 40f;	//投射物速度	https://terraria.fandom.com/wiki/Velocity
 			Item.knockBack = 2.75f;
-			Item.width = 20;
+			Item.width = 20;	//碰撞箱
 			Item.height = 12;
 			Item.damage = 23;
-			Item.axe = 14;
+			Item.axe = 14;	//數值乘5倍?
 			Item.UseSound = SoundID.Item23;
-			Item.shoot = ModContent.ProjectileType<usepochita>();
-			Item.rare = 4;
-			Item.value = 54000;
-			Item.noMelee = true;
-			Item.noUseGraphic = true;
+			Item.shoot = ModContent.ProjectileType<usepochita>();	//物品的投射物
+			Item.rare = 4;	//稀有度
+			Item.value = 54000;	//價值
+			Item.noMelee = true;	//物品無碰撞傷害
+			Item.noUseGraphic = true;	//物品不顯示圖片
 			//Item.melee = true;
-			Item.channel = true;
+			Item.channel = true;	//按著右鍵有特殊效果
 		}
         public override bool AltFunctionUse(Player player)
         {
